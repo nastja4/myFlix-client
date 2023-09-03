6cfe1f2c538f99eca333f61e44b6a92309c69eee
Anastasia Fazius <AnastasiaFazius@gmx.de>
2023-09-03 23:31:51 +0200

    Adds connection with the API, prop-types

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -5,7 +7,29 @@ export const MovieCard = ({ movie, onMovieClick }) => {
         onMovieClick(movie);
       }}
     >
-      {movie.title}
+      {movie.Title}
     </div>
   );
+};
+
+// // Here is where we define all the props constraints for the MovieCard
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Featured: PropTypes.bool.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired,
+      Death: PropTypes.string.isRequired,
+    }).isRequired,    
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,22 +1,23 @@
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
-      <h1>{movie.title}</h1>
-      <img src={movie.image} alt={movie.title} style={{ width: "50%", height: "auto" }} />
-      <h3>{movie.description}</h3>
+      <h1>{movie.Title}</h1>
+      <img src={movie.ImagePath} alt={movie.Title} style={{ width: "30%", height: "auto" }} />
+      <h3>{movie.Description}</h3>
       <div>
         <h4>Genre:</h4>
-        <p>{movie.genre.name}</p>
-        <p>Description: {movie.genre.description}</p>
+        <p>{movie.Genre.Name}</p>
+        <p>Description: {movie.Genre.Description}</p>
       </div>
       <div>
         <h4>Director:</h4>
-        <p>{movie.director.name}</p>
-        <p>Bio: {movie.director.bio}</p>
-        <p>Birth year: {movie.director.birth}</p>
-        <p>Death year: {movie.director.death}</p>
+        <p>{movie.Director.Name}</p>
+        <p>Bio: {movie.Director.Bio}</p>
+        <p>Birth year: {movie.Director ? movie.Director.Birth || 'Birth Year Not Available' : 'Birth Year Not Available'}</p>
+        <p>Death year: {movie.Director.Death}</p>
+        {/* <p>Featured: {movie.featured ? 'Yes' : 'No'}</p> */}
       </div>
-      {/* <p>Featured: {movie.featured ? 'Yes' : 'No'}</p> */}
+      
       
       <button onClick={onBackClick}>Back to the movie list</button>
     </div>
